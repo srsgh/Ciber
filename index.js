c46dc72f90222e8aded6d956cde2515a07323eb1
srsgh <theshrutirawat@gmail.com>
2021-12-05 14:12:49 +0530

    changes made in index.js ./src/aws-exports to ./aws-exports.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import App from './App';
 import {name as appName} from './app.json';
 
 import Amplify from 'aws-amplify';
-import config from './src/aws-exports';
+import config from './aws-exports';
 Amplify.configure(config);
 
 AppRegistry.registerComponent(appName, () => App);
