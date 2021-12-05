bef832f1ccf8ee9f6d96569937f7422500e97381
srsgh <theshrutirawat@gmail.com>
2021-12-06 03:02:48 +0530

    nuked aws whole. Onto try-3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "ciber": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
