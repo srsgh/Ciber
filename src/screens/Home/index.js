35b92abfe204bf8f3d6830cfae1de292e2503fd5
srsgh <theshrutirawat@gmail.com>
2021-12-06 06:52:31 +0530

    Added NYProfile.NOT YOUR PROFILE.to chekout other's profiles.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, FlatList, View, Dimensions} from 'react-native';
 import posts from '../../assets/data/posts';
 import Post from '../../components/CiberPost';
 import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
-
+import {API, graphqlOperation} from 'aws-amplify';
+import {listPosts} from '../../graphql/queries';
 const Home = () => {
   const tabBarHeight = useBottomTabBarHeight();
+
+  //first mount
+  useEffect(() => {
+    effect;
+    return () => {
+      cleanup;
+    };
+  }, [input]);
+
   return (
     <View>
       <FlatList
